c805639e706a5978d90969727e63884cc404bc95
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-05-12 16:26:53 -0400

    fixing tailwind warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+// tailwind.config.js
 module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   theme: {},
   variants: {},
   plugins: []
