dc25a5d03953413732c0213360deb06bd9998940
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-05-12 16:31:59 -0400

    updating tailwind and docker-compose

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+  purge: ['./**/*.html', './**/*.vue', './**/*.jsx'],
   theme: {},
   variants: {},
   plugins: []
