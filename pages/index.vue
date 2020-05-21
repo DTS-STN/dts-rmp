8331d48cbf85063baabf14e89e458d676aa403a5
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-05-21 17:08:33 -0400

    replacing kmp by rmp everywhere

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        dts-kmp
+        dts-rmp
       </h1>
       <h2 class="subtitle">
         knowledge management portal proof of concept!
