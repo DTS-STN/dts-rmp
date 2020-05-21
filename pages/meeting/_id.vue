5d3eba65fdc142b6886587841717cd1f0ad64ff2
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-05-21 13:30:34 -0400

    adding skeleton for meetings pages

---
+++ pages/meeting/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Display a meeting by id</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Display a meeting by id',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Display a meeting!'
+        }
+      ]
+    }
+  }
+}
+</script>
