2fd072558b6526c3fa9d7afe1e92aa57ee2c9e77
Mr. Pond <32133001+P0NDER0SA@users.noreply.github.com>
2020-05-21 14:22:14 -0400

    Update index.vue
    testing deployment

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         dts-kmp
       </h1>
       <h2 class="subtitle">
-        knowledge management portal proof of concept
+        knowledge management portal proof of concept!
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
