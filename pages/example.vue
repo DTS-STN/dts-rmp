e01ebd5bba290c40253b5167651f95f11de82593
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-06-10 15:51:38 -0400

    adding vue2-editor for the notes/comments

---
+++ pages/example.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h2>Is SSR? {{ isSSR }}</h2>
+
+    <vue-editor v-model="content" placeholder="Write Something..." />
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      content: '',
+      pageIsMounted: false,
+      isSSR: !!process.server
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: white;
+  min-height: 35vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
