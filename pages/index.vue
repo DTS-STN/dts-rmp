e43824bedeeac8e5d1410d24df5693325dfca2bc
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-06-17 13:18:25 -0400

    code complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,12 @@
 <template>
-  <div class="main">
-    <div>
-      <h1 class="title">
-        dts-rmp
-      </h1>
-
-      <h2 class="subtitle">
-        relationship management portal proof of concept!
-      </h2>
-    </div>
-  </div>
+  <div />
 </template>
 
 <script>
-export default {}
+export default {
+  // a simple route push to set default page for app
+  fetch() {
+    this.$router.push({ path: 'search/engagement' })
+  }
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.main {
-  min-height: 100vh;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-  @apply pt-12 text-center;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
