9d3e2e06c169a7a61cec0a591f5f8643d6a6c2fa
will0684 <jordan.walcaraz@gmail.com>
2020-06-18 13:35:06 -0400

    Updated required fields

--- server/api/models/engagement.js
+++ server/api/models/engagement.js
@@ -4,7 +4,7 @@ const EngagementSchema = new Schema({
   type: { type: String, required: true },
   date: { type: Date, required: true },
   description: { type: String, required: true },
-  numParticipants: { type: Number },
+  numParticipants: { type: Number, required: true },
   contacts: [{ type: Schema.Types.ObjectId, required: true, ref: 'Contact' }],
   policyProgram: { type: String },
   comments: [{ user: String, content: String, date: Date }],
