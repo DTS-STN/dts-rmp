e30d8418c708ee56caa663c31c402a16714acb1a
Shawn <azuneko1110@gmail.com>
2020-06-18 21:12:48 -0400

    added color change effect on navigation button

--- components/app/AppNavBtn.vue
+++ components/app/AppNavBtn.vue
@@ -1,16 +1,48 @@
 <template>
   <div class="app">
-    <button class="switch add">
+    <button
+      class="switch add"
+      :style="{ color: txtColorAdd, 'background-color': bgColorAdd }"
+      @click="colorChange(true)"
+    >
       <h2>New engagement</h2>
     </button>
-    <button class="switch search">
+    <button
+      class="switch search"
+      :style="{ color: txtColorSearch, 'background-color': bgColorSearch }"
+      @click="colorChange(false)"
+    >
       <h2>Search contacts & engagements</h2>
     </button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      txtColorAdd: '',
+      bgColorAdd: '',
+      txtColorSearch: '',
+      bgColorSearch: ''
+    }
+  },
+  methods: {
+    colorChange(select) {
+      if (select === true) {
+        this.txtColorAdd = '#D87C4F'
+        this.bgColorAdd = 'white'
+        this.bgColorSearch = '#D87C4F'
+        this.txtColorSearch = 'white'
+      } else {
+        this.txtColorAdd = 'white'
+        this.bgColorAdd = '#D87C4F'
+        this.bgColorSearch = 'white'
+        this.txtColorSearch = '#D87C4F'
+      }
+    }
+  }
+}
 </script>
 
 <style>
@@ -28,10 +60,12 @@ button.switch {
 }
 
 button.add {
-  @apply float-left bg-white text-orange-600;
+  @apply float-left text-orange-600;
+  background-color: white;
 }
 
 button.search {
-  @apply float-right bg-orange-600 text-white;
+  @apply float-right text-white;
+  background-color: #d87c4f;
 }
 </style>
