48db1ed67dd875ab1f4c5068da230defa44a7fe5
Benoit Cloutier <cloutier.ben@yahoo.com>
2020-06-22 11:45:40 -0400

    Started Dashboard page.

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    Dashboard
+  <div class="px-10 py-5">
+    <div class="flex w-full">
+      <div class="w-3/4">
+        <h2 class="text-xl">
+          Engagement Dashboard
+        </h2>
+      </div>
+      <div class="w-1/4">
+        <!-- <AppButton /> -->
+        <input id="pdfButton" type="button" value="Download PDF" />
+      </div>
+    </div>
+    <div class="flex">
+      <form class="flex">
+        <select id="time" name="time">
+          <option value="sixMonths">
+            Last 6 months
+          </option>
+          <option value="threeMonths">
+            Last 3 months
+          </option>
+          <option value="oneMonth">
+            Last month
+          </option>
+        </select>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -8,4 +33,11 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+#pdfButton {
+  background-color: #246880;
+  color: white;
+  border-radius: 8px;
+  padding: 12px 25px;
+}
+</style>
