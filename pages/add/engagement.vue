ccc9f6603cd70ae98337507be10358d85917239e
Kevin-Work <nghe0001@algonquinlive.com>
2020-06-24 21:05:42 -0700

    form

---
+++ pages/add/engagement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <EngForm title="Create new engagement" />
+  </div>
+</template>
+
+<script>
+import EngForm from '@/components/engagement/EngForm'
+
+export default {
+  components: {
+    EngForm
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  @apply text-rmp-md-blue text-left tracking-wide font-extrabold text-4xl pt-4;
+}
+</style>
