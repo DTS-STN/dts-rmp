51fbd5438c2a7288e63eb80a1e624de1dfb727ef
Shawn <azuneko1110@gmail.com>
2020-06-26 12:30:40 -0400

    combined the components

--- server/api/engagement/index.js
+++ server/api/engagement/index.js
@@ -1,35 +1,40 @@
 import { Router, json } from 'express'
 import consola from 'consola'
-
 // engagement Model
 import Engagement from '../models/engagement'
-
 const app = Router()
 app.use(json())
-
 // @route   GET api/engagement to do
 // @desc    Gets all engagement  to do
 // @access  Public to do
-
+app.get('/engagements', async(req, res) => {
+  try {
+    const engagements = await Engagement.find()
+    if (!engagements) {
+      consola.error('No engagements exist')
+      throw new Error('No engagements exist')
+    }
+    // implicit else
+    res.json(engagements)
+  } catch (e) {
+    res.status(400)
+    consola.error(e.message)
+  }
+})
 // @route   POST api/engagement/add
 // @desc    Post creates a new engagement
 // @access  Public
-
 // eslint-disable-next-line space-before-function-paren
 app.post('/addEngagement', async (req, res) => {
   let errMessage = ''
-
   try {
     const newEngagement = new Engagement(req.body.engagementDetail)
-
     const savedEngagementDetail = await newEngagement.save()
-
     if (!savedEngagementDetail) {
       consola.error('There was an error creating a new Engagement ')
       errMessage = 'A new Engagement could not be created try again later'
       throw new Error(errMessage)
     }
-
     res.status(200).json({
       engagement: {
         id: savedEngagementDetail._id
@@ -40,5 +45,4 @@ app.post('/addEngagement', async (req, res) => {
     res.status(401).json({ message: errMessage })
   }
 })
-
 export default app
