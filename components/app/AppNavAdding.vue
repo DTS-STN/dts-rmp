b571e750d3d80cbb3892bc1904f662be5c29a971
Benoit Cloutier <cloutier.ben@yahoo.com>
2020-07-08 15:57:13 -0400

    Fixed font sizing.

--- components/app/AppNavAdding.vue
+++ components/app/AppNavAdding.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="navContainer" class="ml-12">
-    <div class="mt-24">
+    <div class="mt-16">
       <h2 class="newAdd font-display">
         {{ $t('app.add') }}
       </h2>
@@ -26,8 +26,8 @@ export default {
 <style>
 .newAdd {
   font-weight: 600;
-  font-size: 48pt;
-  color: #246880;
+  font-size: 48px;
+  color: #426177;
 }
 .requireFields {
   font-weight: 700;
