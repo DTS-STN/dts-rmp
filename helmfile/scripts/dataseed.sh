da74cc1fda6b1d1d6a406ea22a474cd9a52a0d6c
Ben Larabie <benlarabie@gmail.com>
2020-07-09 12:20:26 -0400

    moving dataseed to scripts

--- helmfile/scripts/dataseed.sh
+++ helmfile/scripts/dataseed.sh
@@ -2,4 +2,5 @@
 while [ $(kubectl get pods -l app.kubernetes.io/instance=rmp-$TARGET -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do 
     echo "waiting for pod" && sleep 1; 
 done
-kubectl exec deployment.apps/rmp-$TARGET-dts-rmp -n rmp-$TARGET npm run seed
+PODNAME=$(kubectl get pods --namespace rmp-$TARGET | grep rmp-$TARGET-dts-rmp | awk '{print $1}')
+kubectl exec $PODNAME -n rmp-$TARGET -- npm run seed
--- helmfile/scripts/deleteDatabase.sh
+++ helmfile/scripts/deleteDatabase.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+pushd ../
 helm delete rmp-db-$TARGET --namespace rmp-$TARGET
-cd ./helmfile
 set +x
 . ./context-dev.sh
 set -x
 helmfile --environment $TARGET --selector app=rmp,tier=database apply
-while [ $(kubectl get pods -l app.kubernetes.io/instance=rmp-$TARGET -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do 
-    echo "waiting for pod" && sleep 1; 
-done
+while [[ $(kubectl get pods -l release=rmp-db-$TARGET -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
+popd
