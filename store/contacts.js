eaf8c2a4e3200cd51b16ee8b4d83efd1ac614688
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-07-09 12:43:29 -0400

    checking for logging

--- store/contacts.js
+++ store/contacts.js
@@ -14,6 +14,10 @@ export const mutations = {
 
 export const actions = {
   fetchContacts({ commit }) {
+    // eslint-disable-next-line no-console
+    console.log('client side baseURL: ' + this.$axios.defaults.baseURL)
+    // eslint-disable-next-line no-console
+    console.log('env variable baseURL: ' + process.env.baseURL)
     return this.$axios.get('/api/contact/contacts')
       .then((response) => {
         commit('SET_CONTACTS', response.data)
