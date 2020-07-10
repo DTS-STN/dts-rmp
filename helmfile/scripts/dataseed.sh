cce9eb1fae44667db0662d583111b27cb1b8cd58
Ben Larabie <benlarabie@gmail.com>
2020-07-10 10:37:31 -0400

    adding docker tag filter to status check

--- helmfile/scripts/dataseed.sh
+++ helmfile/scripts/dataseed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $WORKSPACE/helmfile
 pwd
-while [ $(kubectl get pods -l app.kubernetes.io/instance=rmp-$TARGET -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do 
+while [ $(kubectl get pods -l app.kubernetes.io/instance=rmp-$TARGET,docker_tag=$DOCKER_TAG -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do 
     echo "waiting for pod" && sleep 1; 
 done
 PODNAME=$(kubectl get pods --namespace rmp-$TARGET | grep rmp-$TARGET-dts-rmp | awk '{print $1}')
