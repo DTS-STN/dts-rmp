cac0ab019d4dfceab7058c4185de5ded78f7845a
will0684 <jordan.walcaraz@gmail.com>
2020-07-21 18:16:06 -0400

    Fixed Contact list disappearing on refresh

--- components/engagement/EngSelectContacts.vue
+++ components/engagement/EngSelectContacts.vue
@@ -85,7 +85,7 @@ export default {
     contacts: state => state.contacts.contacts
   }),
 
-  created() {
+  beforeMount() {
     this.contactProps.forEach(contact => this.showContact(contact, true))
   },
 
