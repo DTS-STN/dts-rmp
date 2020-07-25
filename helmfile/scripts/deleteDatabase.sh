d013251d37097c07da3fa395c3b6ba73ac40c162
Ben Larabie <benlarabie@gmail.com>
2020-07-25 13:30:15 -0400

    Adding prod context to delete database

--- helmfile/scripts/deleteDatabase.sh
+++ helmfile/scripts/deleteDatabase.sh
@@ -3,7 +3,20 @@ cd $WORKSPACE/helmfile
 pwd
 helm delete rmp-db-$TARGET --namespace rmp-$TARGET
 set +x
-. ./context-dev.sh
+if [ "$TARGET" = "prod-blue" ] || [ "$TARGET"= "prod-green" ]
+    then
+    echo "Prod"
+    set +x
+    echo "Setting Environment Secrets. This is obfuscated"
+    . ./context-prod.sh > /dev/null
+    set -x
+else
+    echo "Dev"
+    set +x
+    echo "Setting Environment Secrets. This is obfuscated"
+    . ./context-dev.sh > /dev/null
+    set -x
+fi 
 set -x
 helmfile --environment $TARGET --selector app=rmp,tier=database apply
 while [[ $(kubectl get pods -l release=rmp-db-$TARGET -n rmp-$TARGET -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
